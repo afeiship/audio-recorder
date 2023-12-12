af7d75ea44376f2529b6f2064b10b0f516e18b8e
afeiship <1290657123@qq.com>
2023-12-13 00:52:02 +0800

    feat: rename to watchVolumn

--- src/index.ts
+++ src/index.ts
@@ -1,52 +1,39 @@
 import Recorder from 'js-audio-recorder';
 
-interface RecorderConfig {
-  sampleBits?: number; // 采样位数
-  sampleRate?: number; // 采样率
-  numChannels?: number; // 声道数
-  compiling?: boolean; // 是否边录边播
-}
-const AudioContext = window.AudioContext;
-
 class AudioRecorder extends Recorder {
-  constructor(options: RecorderConfig = {}) {
-    super(options);
-    this.onplayprogress = () => {};
-    this.listenPlayProgress();
-  }
-  async listenPlayProgress() {
-    let timer: any = null;
-    const self = this;
-    if (this.isplaying) {
-      const audioContext = new AudioContext();
-      const wav = this.getWAV();
-      const soundBuffer = wav.buffer;
-      const sampleBuffer = await audioContext.decodeAudioData(soundBuffer);
-      const analyser = audioContext.createAnalyser();
-      const sampleSource = audioContext.createBufferSource();
-      sampleSource.buffer = sampleBuffer;
-      const bufferLength = analyser.frequencyBinCount;
-      const dataArray = new Uint8Array(bufferLength);
+  static async watchVolumn(inAudioBuffer: any, onChange: any) {
+    const audioContext = new AudioContext();
+    const soundBuffer = inAudioBuffer;
+    const sampleBuffer = await audioContext.decodeAudioData(soundBuffer);
+    const analyser = audioContext.createAnalyser();
+    const sampleSource = audioContext.createBufferSource();
+    sampleSource.buffer = sampleBuffer;
+    const bufferLength = analyser.frequencyBinCount;
+    const dataArray = new Uint8Array(bufferLength);
 
-      sampleSource.connect(analyser);
-      analyser.connect(audioContext.destination);
-      sampleSource.start();
+    sampleSource.connect(analyser);
+    analyser.connect(audioContext.destination);
+    sampleSource.start();
 
-      function caclculateVolume() {
-        analyser.getByteFrequencyData(dataArray);
+    function caclculateVolume() {
+      analyser.getByteFrequencyData(dataArray);
 
-        let sum = 0;
-        for (const amplitude of dataArray) {
-          sum += amplitude * amplitude;
-        }
-        const volume = Math.sqrt(sum / dataArray.length);
-        self.onplayprogress(volume);
+      let sum = 0;
+      for (const amplitude of dataArray) {
+        sum += amplitude * amplitude;
       }
-
-      timer = setInterval(caclculateVolume, 500);
-    } else {
-      // clearInterval(timer);
+      const volume = Math.sqrt(sum / dataArray.length);
+      onChange(volume);
     }
+
+    let timer = setInterval(caclculateVolume, 500);
+
+    return {
+      destroy() {
+        onChange(0);
+        clearInterval(timer);
+      },
+    };
   }
 }
 
